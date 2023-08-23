df33d4e9521249242c8eab5844d7ef60e7a7d917
Chen.guangji <chen.guangji@outlook.com>
2023-08-23 14:33:31 +0800

    update

--- script/bin/cal_mafft_identity.sh
+++ script/bin/cal_mafft_identity.sh
@@ -14,7 +14,7 @@ Pwd=`pwd`
 #faname=$(basename $fa)
 source ${SHELL_FOLDER}/config.txt
 
-cat ${shell} | sed 's#/hwfssz5/ST_DIVERSITY/B10K/PUB/local/basic_bio-tool/mafft-7.475/bin/ginsi#/hwfssz1/ST_EARTH/Reference/ST_DIVERSITY/PUB/USER/chenguangji/software/miniconda3/bin/mafft-ginsi#'g | awk -F ';' '{print $1" && echo "NR" done"}' > ${shell}.echo.shell
+cat ${shell} | awk -F ';' '{print $1" && echo "NR" done"}' > ${shell}.echo.shell
 cat ${shell} | awk '{gsub(">","",$6);print "'${perl}' '${mafftIdentity}' "$6" && echo "NR" done"}'> ${shell}.identity.shell
 ${parallel} -j 96 < ${shell}.echo.shell > ${shell}.echo.log && echo mafft done;
 ${parallel} -j 96 < ${shell}.identity.shell > ${id}.mafft.identity.log && echo identity done && grep -v "done" ${id}.mafft.identity.log > ${id}.mafft.identity.out;
